5cacb9d1e3ec24cadc1f5bf2cfcbe9779e7cae1a
adriabama06 <adriabama06@gmail.com>
2022-11-18 11:34:01 +0100

    All work fine

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
+#include "include/bitmap.h"
+
 #include <stdio.h>
 
 int main(int argc, const char** argv)
 {
-    printf("Hello World\n");
+    FILE* to_open = fopen(argv[1], "rb");
+
+    BITMAP bmp;
+
+    bitmap_load(to_open, &bmp);    
+
+    fclose(to_open);
+
+    
+    
+    FILE* to_save = fopen(argv[2], "wb");
+
+    bitmap_save(to_save, &bmp);
+
+    fclose(to_save);
 
     return 0;
 }
